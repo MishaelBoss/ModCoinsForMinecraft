c90cb1960992b2b5389f5bab94ce9596fbf84506
Michael <michaelvoov@yandex.ru>
2026-07-09 13:53:49 +0500

    1

--- src/main/java/com/michaelboss/coinsmod/item/tab/ModCreativeModeTabs.java
+++ src/main/java/com/michaelboss/coinsmod/item/tab/ModCreativeModeTabs.java
@@ -5,7 +5,6 @@ import com.michaelboss.coinsmod.registry.ModItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -20,7 +19,6 @@ public class ModCreativeModeTabs {
 
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.examplemod"))
-            .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> ModItems.COPPER_COIN.get().getDefaultInstance())
             .displayItems((parameters, output) ->
                 ModItems.ITEMS.getEntries().forEach(entry -> output.accept(entry.get()))
